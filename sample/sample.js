0568d8e0c2e536684ecbb03985118cea2b4748b8
Thiago Bonfim <thiakau@gmail.com>
2019-03-28 10:47:46 -0700

    Add Style for default view

--- sample/sample.js
+++ sample/sample.js
@@ -1,21 +1,25 @@
-(
-    function(){
-        this.findEpisode = function(){
-            var inputValue = document.getElementById("episodeName").value;
-            var episode = new BlackMirrorEpisodesHelper(inputValue);
+(function(){
+    this.findEpisode = function(){
+        var inputValue = document.getElementById("episodeName").value;
+        var episode = new BlackMirrorEpisodesHelper(inputValue);
 
-            var target = document.querySelector('#container');
-            var div = document.createElement('div');
+        var oldContent = document.querySelector('.wrapper');
+        if (oldContent) {
+            oldContent.remove();
+        }
 
-            if (episode.currentEpisode.hasOwnProperty('description') && episode.currentEpisode.hasOwnProperty('image')){
-                div.innerHTML = "<img src='"+ episode.currentEpisode.image+"' />";
-                div.innerHTML += "<p class='description'>" + episode.currentEpisode.description+ "</p>";
-            }
-            else {
-                div.innerHTML = "<div class='notFound'>Not found </div>";
-            }
+        var target = document.querySelector('#container');
+        var div = document.createElement('div');
+        div.classList.add('wrapper');
 
-            target.parentNode.insertBefore( div, target.nextSibling );
-        }   
-    }()
-);
+        if (episode.currentEpisode.hasOwnProperty('description') && episode.currentEpisode.hasOwnProperty('image')){
+            div.innerHTML = "<img src='"+ episode.currentEpisode.image+"' />";
+            div.innerHTML += "<p class='description'>" + episode.currentEpisode.description+ "</p>";
+        }
+        else {
+            div.innerHTML = "<div class='notFound'>Not found </div>";
+        }
+
+        target.parentNode.insertBefore( div, target.nextSibling );
+    }   
+}());
