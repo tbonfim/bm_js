dd03fa867a20abb945f02f8374dc617eeabca052
Thiago Bonfim <thiakau@gmail.com>
2019-04-09 15:24:03 -0700

    Changing string pattern

--- sample/sample.js
+++ sample/sample.js
@@ -3,16 +3,16 @@
         var inputValue = document.getElementById("episodeName").value;
         var episode = new BlackMirrorEpisodesHelper(inputValue);
 
-        var oldContent = document.querySelector('.wrapper');
+        var oldContent = document.querySelector(".wrapper");
         if (oldContent) {
             oldContent.remove();
         }
 
-        var target = document.querySelector('#container');
-        var div = document.createElement('div');
-        div.classList.add('wrapper');
+        var target = document.querySelector("#container");
+        var div = document.createElement("div");
+        div.classList.add("wrapper");
 
-        if (episode.currentEpisode.hasOwnProperty('description') && episode.currentEpisode.hasOwnProperty('image')){
+        if (episode.currentEpisode.hasOwnProperty("description") && episode.currentEpisode.hasOwnProperty("image")){
             div.innerHTML = "<img src='"+ episode.currentEpisode.image+"' />";
             div.innerHTML += "<p class='description'>" + episode.currentEpisode.description+ "</p>";
         }
